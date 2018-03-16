4684aab4e7ee1ee884487f021dd10c897f19e885
Vyacheslav Lapin <Vyachesl@vLapin.ru>
2018-03-16 17:14:28 +0300

    Mockito

--- src/main/java/com/epam/spb/javalab/mar/Main.java
+++ src/main/java/com/epam/spb/javalab/mar/Main.java
@@ -1,9 +1,21 @@
 package com.epam.spb.javalab.mar;
 
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
+
+import java.io.InputStream;
+import java.util.Properties;
 
 @UtilityClass
 public class Main {
 
-
+    @SneakyThrows
+    public static void main(String... args) {
+        Properties properties = new Properties();
+        try (InputStream resourceAsStream = Main.class.getResourceAsStream("/Prop.properties")) {
+            properties.load(resourceAsStream);
+        }
+        String xyz = properties.getProperty("xyz", "1");
+        System.out.println(xyz);
+    }
 }
